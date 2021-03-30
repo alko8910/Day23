34f0aea9abfbce11d56e093948feedd9ec82923b
alko8910 <almedinemkic@gmail.com>
2021-03-30 22:50:25 +0200

    Added Day23

---
+++ ex04/src/index.js
@@ -0,0 +1,10 @@
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App';  // Add this line
+import "./index.css";
+
+// Remove code below this line
+
+// Remove code below this line
+ReactDOM.render(<App />, document.getElementById("root"));
